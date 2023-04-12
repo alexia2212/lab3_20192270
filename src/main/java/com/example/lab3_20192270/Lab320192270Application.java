9db1914247c9225e537417f8d41cf7a95e54a086
alexia22012 <alexia_jg@outlook.es>
2023-04-11 20:45:49 -0500

    first commit

---
+++ src/main/java/com/example/lab3_20192270/Lab320192270Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20192270;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320192270Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320192270Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3_20192270/Lab320192270ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20192270;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320192270ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
